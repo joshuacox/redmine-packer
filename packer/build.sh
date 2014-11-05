edc87658f63b05fd2490b6ede1edfd94a4c50fc0
Josh Cox <josh@webhosting.coop>
2014-11-05 13:18:32 -0600

    added simple sed to build.sh to add in my apt-cacher, but remove it afterwards to keep it clean for everyone else

--- packer/build.sh
+++ packer/build.sh
@@ -2,10 +2,12 @@
 rm -r vendor
 rm Berksfile.lock
 berks vendor vendor/cookbooks
+sed -i 's/d-i\ mirror\/http\/proxy\ string/d-i\ mirror\/http\/proxy\ string\ http:\/\/65.67.51.187:3142/' http/debian-7/preseed.cfg
 #/usr/bin/time -v packer build -debug --only=qemu redmine-debian-7.7-i386.json
 #/usr/bin/time -v packer build --only=qemu redmine-debian-7.7-i386.json
 /usr/bin/time -v packer build --only=virtualbox-iso redmine-debian-7.7-i386.json
 #/usr/bin/time -v packer build --only=virtualbox-iso redmine-debian-7.7-i386.json
 # beeper
+sed -i 's/d-i\ mirror\/http\/proxy\ string\ http:\/\/65.67.51.187:3142/d-i\ mirror\/http\/proxy\ string/' http/debian-7/preseed.cfg
 aplay /usr/share/sounds/alsa/Front_Center.wav
 # ogg123 /usr/share/sounds/KDE-Im-Irc-Event.ogg
