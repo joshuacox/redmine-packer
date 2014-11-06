1216ca8a4e927522dc1feab5427f1bb47049410b
Josh Cox <josh@webhosting.coop>
2014-11-05 19:34:38 -0600

    qemu

--- packer/build.sh
+++ packer/build.sh
@@ -4,10 +4,10 @@ rm Berksfile.lock
 berks vendor vendor/cookbooks
 sed -i 's/d-i\ mirror\/http\/proxy\ string/d-i\ mirror\/http\/proxy\ string\ http:\/\/65.67.51.187:3142/' http/debian-7/preseed.cfg
 #/usr/bin/time -v packer build -debug --only=qemu redmine-debian-7.7-i386.json
-#/usr/bin/time -v packer build --only=qemu redmine-debian-7.7-i386.json
-/usr/bin/time -v packer build --only=virtualbox-iso redmine-debian-7.7-i386.json
+/usr/bin/time -v packer build --only=qemu redmine-debian-7.7-i386.json
 #/usr/bin/time -v packer build --only=virtualbox-iso redmine-debian-7.7-i386.json
-# beeper
+#/usr/bin/time -v packer build --only=virtualbox-iso redmine-debian-7.7-i386.json
 sed -i 's/d-i\ mirror\/http\/proxy\ string\ http:\/\/65.67.51.187:3142/d-i\ mirror\/http\/proxy\ string/' http/debian-7/preseed.cfg
-aplay /usr/share/sounds/alsa/Front_Center.wav
+# beeper
+#aplay /usr/share/sounds/alsa/Front_Center.wav
 # ogg123 /usr/share/sounds/KDE-Im-Irc-Event.ogg
